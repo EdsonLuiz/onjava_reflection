173312f89a9a4167c3c9be3929ab0a5e095f2ac0
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-12-13 06:58:18 -0300

    :construction: ToyTest

---
+++ src/main/java/org/example/reflection/toys/ToyTest.java
@@ -0,0 +1,16 @@
+package org.example.reflection.toys;
+
+interface HasBatteries {}
+interface Waterprof {}
+interface Shoots {}
+
+class Toy {
+    // Comment out the following zero-argument
+    // constructor to see NoSuchMethodError
+    public Toy() {}
+    public Toy(int i) {}
+}
+
+public class ToyTest {
+
+}
