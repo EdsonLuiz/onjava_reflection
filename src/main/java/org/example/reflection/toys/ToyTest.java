304147a5ebaa8a720f66d5f77363ef47d45f47be
Edson Luiz <edson.rodrigues@bs.nttdata.com>
2022-12-14 06:59:36 -0300

    :sparkles: FancyToy

--- src/main/java/org/example/reflection/toys/ToyTest.java
+++ src/main/java/org/example/reflection/toys/ToyTest.java
@@ -11,6 +11,41 @@ class Toy {
     public Toy(int i) {}
 }
 
-public class ToyTest {
+class FancyToy extends Toy implements HasBatteries, Waterprof, Shoots {
+    public FancyToy() {super(1);}
+}
 
+public class ToyTest {
+    static void printInfo(Class cc) {
+        System.out.println("Class name: " + cc.getName() + " is interface? [" + cc.isInterface() + "]");
+        System.out.println("Simple name: " + cc.getSimpleName());
+        System.out.println("Canonical name: " + cc.getCanonicalName());
+    }
+
+    public static void main(String[] args) {
+        Class c = null;
+        try {
+            c = Class.forName("org.example.reflection.toys.FancyToy");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Can't find FancyToy");
+            System.exit(1);
+        }
+        printInfo(c);
+
+        for(Class face : c.getInterfaces())
+            printInfo(face);
+
+        Class up = c.getSuperclass();
+        Object object = null;
+
+        try {
+            // Requires public zero-argument constructor:
+            object = up.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot instantiate");
+        }
+
+        printInfo(object.getClass());
+    }
 }
