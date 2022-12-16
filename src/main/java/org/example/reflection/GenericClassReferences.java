ded051a40ab74713602846f67fc62e5b185aae3b
Edson Luiz <edson.rodrigues@bs.nttdata.com>
2022-12-16 06:27:49 -0300

    :sparkles: GenericClassReferences

---
+++ src/main/java/org/example/reflection/GenericClassReferences.java
@@ -0,0 +1,12 @@
+package org.example.reflection;
+
+public class GenericClassReferences {
+    public static void main(String[] args) {
+        Class intClass = int.class;
+        intClass = double.class;
+
+        Class<Integer> genericIntClass = int.class;
+        genericIntClass = Integer.class; // Same thing
+        // genericIntClass = double.class; // Illegal
+    }
+}
