0c97be50f68cd894700f61b8746ddaef0438fc94
Edson Luiz <edson.rodrigues@bs.nttdata.com>
2022-12-27 06:49:21 -0300

    :sparkles: PetCreator.

---
+++ src/main/java/org/example/reflection/pests/PetCreator.java
@@ -0,0 +1,31 @@
+package org.example.reflection.pests;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Using class literals
+ */
+public class PetCreator extends Creator {
+    // No try block needed
+    public static final List<Class<? extends Pet>> ALL_TYPES =
+            List.of(Pet.class, Dog.class, Cat.class, Rodent.class, Mutt.class, Pug.class, EgyptianMau.class, Manx.class, Cymric.class, Rat.class, Mouse.class, Hamster.class);
+
+    // Types for random creation
+    private static  final List<Class< ? extends Pet>> TYPES = ALL_TYPES.subList(
+            ALL_TYPES.indexOf(Mutt.class),
+            ALL_TYPES.size()
+    );
+
+    @Override
+    public List<Class<? extends Pet>> types() {
+        return TYPES;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(TYPES);
+        List<Pet> pets = new PetCreator().list(7);
+        System.out.println(pets);
+    }
+}
