f15f8266f11454d6ad990a841c1d8f7f0a8cc751
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2023-01-13 07:02:13 -0300

    :sparkles: Operation

---
+++ src/main/java/org/example/reflection/Operation.java
@@ -0,0 +1,13 @@
+package org.example.reflection;
+
+import java.util.function.Supplier;
+
+public class Operation {
+    public final Supplier<String> description;
+    public final Runnable command;
+
+    public Operation(Supplier<String> descr, Runnable cmd) {
+        description = descr;
+        command = cmd;
+    }
+}
